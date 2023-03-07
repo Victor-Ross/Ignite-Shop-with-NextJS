6452cc2d6298bdd77ab51638be3276d2a92f0626
Victor Ross <victor2ross@gmail.com>
2023-03-07 19:49:27 -0300

    finished Ignite shop w/NextJS

---
+++ src/pages/api/checkout.ts
@@ -0,0 +1,34 @@
+import { stripe } from '@/src/lib/stripe';
+import { NextApiRequest, NextApiResponse } from 'next';
+
+export default async function handler(
+  req: NextApiRequest,
+  res: NextApiResponse
+) {
+  if (req.method !== 'POST') {
+    return res.status(405).json({ error: 'Method not allowed' });
+  }
+
+  const { priceId } = req.body;
+
+  if (!priceId) {
+    return res.status(400).json({ message: 'Price not found' });
+  }
+
+  const successUrl = `${process.env.NEXT_URL}/success?session_id={CHECKOUT_SESSION_ID}`;
+  const cancelUrl = `${process.env.NEXT_URL}/`;
+
+  const checkoutSession = await stripe.checkout.sessions.create({
+    mode: 'payment',
+    line_items: [
+      {
+        price: priceId,
+        quantity: 1,
+      },
+    ],
+    success_url: successUrl,
+    cancel_url: cancelUrl,
+  });
+
+  return res.status(201).json({ checkoutUrl: checkoutSession.url });
+}
